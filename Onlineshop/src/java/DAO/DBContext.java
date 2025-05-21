6be1a179dca824ab85f22b6db7c24e0bda8207e9
ducvmhe170394 <ducvmhe170394@fpt.edu.vn>
2025-05-22 03:40:04 +0700

    update connect database, code viewallproduct nhung chua code man

---
+++ Onlineshop/src/java/DAO/DBContext.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/FlowerShopDb";
+            String user = "root";
+            String password = "123";
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+        if (db.connection != null) {
+            System.out.println("Kết nối thành công đến MySQL!");
+        } else {
+            System.out.println("Kết nối thất bại.");
+        }
+    }
+}    
