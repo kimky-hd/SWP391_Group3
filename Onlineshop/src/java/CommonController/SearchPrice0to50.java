e04bdc8200be6db64b7306d41ee4f53eccaf845c
ducvmhe170394 <ducvmhe170394@gmail.com>
2025-06-10 12:56:06 +0700

    pull

--- Onlineshop/src/java/CommonController/SearchPrice0to50.java
+++ Onlineshop/src/java/CommonController/SearchPrice0to50.java
@@ -40,6 +40,9 @@ public class SearchPrice0to50 extends HttpServlet {
         request.setAttribute("productList", list);
         request.setAttribute("listAllColors", listAllColors);
         request.setAttribute("listAllSeasons", listAllSeasons);
+        request.setAttribute("selectedPriceRange", "0to50");
+
+        
         request.getRequestDispatcher("ProductList.jsp").forward(request, response);
     
     } 
