e65d7312f49766fa288e612f4fefb4f33a0f6161
ducvmhe170394 <ducvmhe170394@fpt.edu.vn>
2025-06-18 03:45:35 +0700

    pull

--- Onlineshop/src/java/ManagerController/SearchCategoryByName.java
+++ Onlineshop/src/java/ManagerController/SearchCategoryByName.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package ManagerController;
 
 import DAO.CategoryDAO;
+import Model.Category;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -13,40 +13,44 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  *
  * @author Duccon
  */
-public class UpdateCategoryController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+public class SearchCategoryByName extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet UpdateCategoryController</title>");  
+            out.println("<title>Servlet ViewCategoryListController</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet UpdateCategoryController at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet ViewCategoryListController at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,14 +58,19 @@ public class UpdateCategoryController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-       String categoryID = request.getParameter("categoryID");
-       request.setAttribute("categoryID",categoryID);
-       request.getRequestDispatcher("Manager_UpdateCategory.jsp").forward(request, response);
-    } 
+            throws ServletException, IOException {
+        String txt = request.getParameter("txt").trim();
+        CategoryDAO cateDAO = new CategoryDAO();
+        List<Category> category = cateDAO.getCategoryByName(txt);
+        
+        request.setAttribute("txt", txt);
+        request.setAttribute("catelist", category);
+        request.getRequestDispatcher("Manager_ListCategory.jsp").forward(request, response);
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -69,27 +78,13 @@ public class UpdateCategoryController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        int categoryID = Integer.parseInt(request.getParameter("categoryID"));
-        String categoryName =request.getParameter("categoryName");
-        CategoryDAO catedao =new CategoryDAO();
-        boolean checkduplicate = catedao.CheckDuplicateCategory(categoryName);
-        
-        if(checkduplicate){
-            request.setAttribute("msg", "Thể loại này đã tồn tại!");
-            request.getRequestDispatcher("Manager_UpdateCategory.jsp").forward(request, response);
-        }else{
-            HttpSession session = request.getSession();
-            boolean updatecheck = catedao.update(categoryID,categoryName);
-            if(updatecheck){
-            session.setAttribute("success", "Sửa thành công!");
-            response.sendRedirect("viewcategorylist");
-            }
-        }
+            throws ServletException, IOException {
+        processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
